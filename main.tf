d29fd78d9611d2f6f1412afcf3ec7b7aeb8b0159
Ratan01 <ratanrudra010@gmail.com>
2023-08-31 18:15:17 +0530

    resource name edited

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "azurerm" {
   features {}
 }
 locals {
-  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
-  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rgrp.*.name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rgrp.*.location, [""]), 0)
 
   access_policies = [
     for p in var.access_policies : merge({

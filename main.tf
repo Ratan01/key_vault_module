874a279d10e6697aca19fb77463371d3b9690283
Ratan01 <ratanrudra010@gmail.com>
2023-08-31 17:26:04 +0530

    output.tf file updated

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-
+feature{}
 locals {
   resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
   location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)

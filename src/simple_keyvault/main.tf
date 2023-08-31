6269e2ee66b43f1f5744e722af378063f30c4c4b
Ratan01 <ratanrudra010@gmail.com>
2023-08-31 16:56:31 +0530

    main.tf file updated

--- src/simple_keyvault/main.tf
+++ src/simple_keyvault/main.tf
@@ -1,5 +1,9 @@
 # Azurerm Provider configuration
 provider "azurerm" {
+  subscription_id = "53fc9b60-e863-4a92-a385-97d5cfe2d7ec"
+  client_id       = "1105cb82-7c79-4a90-8822-45dc9f6f1c46"
+  client_secret   = "tPU8Q~1V0gjs6g6exPXtio9vTncCuIBQEa0hqa22"
+  tenant_id       = "0e083f14-f4a1-4950-846c-c36235ef9a6f"
   features {}
 }
 

5215c52f9769d3f1c8283d4556991b75f93761be
Ratan01 <ratanrudra010@gmail.com>
2023-08-31 17:27:48 +0530

    main.tf file updated

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-feature{}
+provider "azurerm" {
+  features {}
+}
 locals {
   resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
   location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)

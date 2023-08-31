45cb6ce5a8ecddbb40136f12771e385522552b51
Ratan01 <ratanrudra010@gmail.com>
2023-08-31 18:11:00 +0530

    resource name added

--- variable.tf
+++ variable.tf
@@ -4,11 +4,13 @@ variable "create_resource_group" {
 }
 
 variable "resource_group_name" {
+  type = string
   description = "A container that holds related resources for an Azure solution"
   default     = "rg"
 }
 
 variable "location" {
+  type = string
   description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
   default     = "east us"
 }
